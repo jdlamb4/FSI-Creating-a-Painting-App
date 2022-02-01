26cf2663d79511d38a59252a31ad57cbf0473df4
Jacob <jacoblamb15@gmail.com>
2022-01-31 21:29:38 -0500

    refratored and made better

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ painting.addEventListener('click', function(e){
     e.target.style.backgroundColor = selectedColor;
 });
 
-let colorChoices = document.querySelectorAll(".color-choice");
-for (let i = 0; i < colorChoices.length; i++) {
-    colorChoices[i].addEventListener('click', function() {
-        selectedColor = colorChoices[i].id;
-    });
-}
+// let colorChoices = document.querySelectorAll(".color-choice");
+// for (let i = 0; i < colorChoices.length; i++) {
+//     colorChoices[i].addEventListener('click', function() {
+//         selectedColor = colorChoices[i].id;
+//     });
+// }
+
+let palette = document.querySelector('.palette');
+palette.addEventListener('click', function(e){
+    selectedColor = e.target.id;
+})
