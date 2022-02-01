afc3c9595e9510969b377d774309fd45e96b1b9c
Jacob <jacoblamb15@gmail.com>
2022-01-31 21:23:33 -0500

    working function

---
+++ index.js
@@ -0,0 +1,13 @@
+let selectedColor = "blue"
+let painting = document.querySelector('.painting');
+
+painting.addEventListener('click', function(e){
+    e.target.style.backgroundColor = selectedColor;
+});
+
+let colorChoices = document.querySelectorAll(".color-choice");
+for (let i = 0; i < colorChoices.length; i++) {
+    colorChoices[i].addEventListener('click', function() {
+        selectedColor = colorChoices[i].id;
+    });
+}
